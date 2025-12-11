822ac523a49dc88542d556ac90507f3f78a6bc1a
codingpoppy94 <yly0617@naver.com>
2025-12-11 18:48:43 +0900

    refactor: 디스코드 bot 헤더 이름 변경

--- src/middlewares/authHandler.ts
+++ src/middlewares/authHandler.ts
@@ -33,7 +33,7 @@ export const verifyAuth = async (
   try {
 
     // --- 봇 검증 --- 
-    const botHeader = req.headers['discord-bot-header'];
+    const botHeader = req.headers['X-Discord-Bot'];
     if (botHeader) {
       if(botHeader !== botSecret){
         throw new BusinessError('Invalid bot secret', 403, {isLoggable: false});
